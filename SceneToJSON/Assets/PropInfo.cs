f65d9374525fb1590cfd77c2879b462228d9af81
Kristian <kristiandixon1206@gmail.com>
2019-08-26 16:30:38 +0100

    Almost ready to output a json file

--- SceneToJSON/Assets/PropInfo.cs
+++ SceneToJSON/Assets/PropInfo.cs
@@ -1,18 +1,104 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+
+[Serializable]
+public class Prop
+{ 
+    public string Name = "";
+    public string Mesh = "";
+    public List<float> Transform;
+    public List<string> HitGroups;
+
+
+    public MetalData Metal;
+    public MatData Material;
+    public WorldData WorldData;
+}
+
+
+[Serializable]
+public class MetalData
+{
+    public string ConstantBufferType = "Metal";
+    public float Reflectivity = 0.5f;
+    public float Scatter = 0;
+}
+
+
+[Serializable]
+public class MatData
+{
+    public string ConstantBufferType = "Material";
+    public List<float> MaterialColour;
+    public List<float> SpecularColour;
+    public float SpecularPower;
+}
+
+[Serializable]
+public class WorldData
+{
+    public string ConstantBufferType = "World";
+}
+
+
 
 public class PropInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Prop prop;
 
-    // Update is called once per frame
-    void Update()
+   
+
+    public bool isMetal;
+    public bool isMaterial = true;
+    public bool isWorld = true;
+
+    private void Awake()
     {
+        prop.Name = gameObject.name;
+
+        
+
+        if(!isMetal)
+            prop.Metal.ConstantBufferType = "";
+
+
+        if (!isMaterial)
+        {
+            prop.Material.ConstantBufferType = "";
+        }
+
+        if(!isWorld)
+            prop.WorldData.ConstantBufferType = "";
+            
+
+
+
+        //Ugly but it works
+        prop.Transform.Add(transform.localToWorldMatrix.m00);
+        prop.Transform.Add(transform.localToWorldMatrix.m01);
+        prop.Transform.Add(transform.localToWorldMatrix.m02);
+        prop.Transform.Add(transform.localToWorldMatrix.m03);
+        prop.Transform.Add(transform.localToWorldMatrix.m10);
+        prop.Transform.Add(transform.localToWorldMatrix.m11);
+        prop.Transform.Add(transform.localToWorldMatrix.m12);
+        prop.Transform.Add(transform.localToWorldMatrix.m13);
+        prop.Transform.Add(transform.localToWorldMatrix.m20);
+        prop.Transform.Add(transform.localToWorldMatrix.m21);
+        prop.Transform.Add(transform.localToWorldMatrix.m22);
+        prop.Transform.Add(transform.localToWorldMatrix.m23);
+        prop.Transform.Add(transform.localToWorldMatrix.m30);
+        prop.Transform.Add(transform.localToWorldMatrix.m31);
+        prop.Transform.Add(transform.localToWorldMatrix.m32);
+        prop.Transform.Add(transform.localToWorldMatrix.m33);
+
+        string jsonOutput = JsonUtility.ToJson(prop);
+
+
+        Debug.Log(jsonOutput);
+
         
     }
 }
---
+++ SceneToJSON/Assets/Saver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Serialization;
+
+[Serializable]
+public class Props
+{
+    public List<Prop> GameObjects = new List<Prop>();
+}
+
+public class Saver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get all children and write json file with them.
+        Props props = new Props();
+
+        foreach(PropInfo info in GetComponentsInChildren<PropInfo>())
+        {
+            props.GameObjects.Add(info.prop);
+        }
+
+        Debug.Log(JsonUtility.ToJson(props));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
